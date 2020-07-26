37c560ca180846a2e234a8f21f62e7bd5f9b5f2f
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-26 04:07:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,13 +4,8 @@
 #   "Amanda" => 27
 # }
 
-def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-
-  winner 
 end
